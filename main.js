155b6ae577b00c93c11e1000250a20412985e73d
Jen <jfong@mozilla.com>
2016-01-04 15:27:48 -0800

    use req anim frame

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
   var ctxHeight = canvas.height = window.innerHeight;
   var fov = 150;
   var ctx = canvas.getContext('2d');
+  var start = window.mozAnimationStartTime || new Date().getTime();
   var pixels = [];
 
   function generate() {
@@ -41,43 +42,47 @@
       }
     }
 
-    function render() {
-      ctx.clearRect(0, 0, ctxWidth, ctxHeight);
-      var imagedata = ctx.getImageData(0, 0, ctxWidth, ctxHeight);
-      var i = pixels.length;
+    render();
+  }
 
-      while (i--) {
-        var pixel = pixels[i];
-        //calculating 2d position for 3d coordinates
-        //fov = field of view = denotes how far the pixels are from us.
-        //the scale will control how the spacing between the pixels will decrease with increasing distance from us.
-        var scale = fov / (fov + pixel.z);
-        var x2d = pixel.x * scale + ctxWidth / 2;
-        var y2d = pixel.y * scale + ctxHeight / 2;
+  function render() {
+    ctx.clearRect(0, 0, ctxWidth, ctxHeight);
+    var imagedata = ctx.getImageData(0, 0, ctxWidth, ctxHeight);
+    var i = pixels.length;
 
-        //marking the points only if they are inside the screen
-        if (x2d >= 0 && x2d <= ctxWidth && y2d >= 0 && y2d <= ctxHeight) {
-          var rad = ctx.createRadialGradient(x2d + 25, y2d + 25,  1, x2d + 50, y2d + 50, 50);
-          rad.addColorStop(1, 'transparent');
-          rad.addColorStop(0.1, 'rgba(255, 255, 255, 1.0)');
-          rad.addColorStop(0.2, 'rgba(255, 255, 255, 0.1)');
-          rad.addColorStop(0.3, 'rgba(255, 0, 133, 0.5)');
-          rad.addColorStop(0.6, 'rgba(255, 0, 133, 0.3)');
-          rad.addColorStop(0.7, 'rgba(255, 0, 133, 0.1)');
-          ctx.fillStyle = rad;
-          ctx.fillRect(x2d, y2d, 100, 100);
-        }
+    while (i--) {
+      var pixel = pixels[i];
+      //calculating 2d position for 3d coordinates
+      //fov = field of view = denotes how far the pixels are from us.
+      //the scale will control how the spacing between the pixels will decrease with increasing distance from us.
+      var scale = fov / (fov + pixel.z);
+      var x2d = pixel.x * scale + ctxWidth / 2;
+      var y2d = pixel.y * scale + ctxHeight / 2;
 
-        pixel.z -= 1;
+      //marking the points only if they are inside the screen
+      if (x2d >= 0 && x2d <= ctxWidth && y2d >= 0 && y2d <= ctxHeight) {
+        var rad = ctx.createRadialGradient(x2d + 25, y2d + 25,  1, x2d + 50, y2d + 50, 50);
+        rad.addColorStop(1, 'transparent');
+        rad.addColorStop(0.1, 'rgba(255, 255, 255, 1.0)');
+        rad.addColorStop(0.2, 'rgba(255, 255, 255, 0.1)');
+        rad.addColorStop(0.3, 'rgba(255, 0, 133, 0.5)');
+        rad.addColorStop(0.6, 'rgba(255, 0, 133, 0.3)');
+        rad.addColorStop(0.7, 'rgba(255, 0, 133, 0.1)');
+        ctx.fillStyle = rad;
+        ctx.fillRect(x2d, y2d, 100, 100);
+      }
 
-        if (pixel.z < -fov) {
-          pixel.z += 5 * fov;
-        }
+      pixel.z -= 1;
+
+      if (pixel.z < -fov) {
+        pixel.z += 5 * fov;
       }
     }
 
-    setInterval(render, 1000 / 30);
+    setTimeout(function () {
+      requestAnimationFrame(render);
+    }, 1000 / 30);
   }
 
-  generate();
+  generate(start);
 })();
