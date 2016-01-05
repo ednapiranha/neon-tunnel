db3173f84c69342b1f4a3e63549c2fcd662237b5
Jen <jfong@mozilla.com>
2016-01-04 21:55:17 -0800

    randomize an array of tracks

--- youtube.js
+++ youtube.js
@@ -5,12 +5,31 @@
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
   var player;
+  var videoArray = ['en-HmMPpze4', 'MYlaH763igI', '49P4GUA4AIY', 'CGjm61lZ2W8', 'JCUPc9zVfyo'];
+
+  videoArray = shuffle(videoArray);
+
   function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      videoId: 'MYlaH763igI',
+      height: '190',
+      width: '340',
+      videoId: videoArray[0],
       events: {
         'onReady': onPlayerReady
       }
